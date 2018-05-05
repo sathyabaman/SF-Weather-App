b15dbbd90f04eff05b06df85f6f30ef171f855aa
sathya baman <sathyabaman.k@viewqwest.com>
2018-05-05 13:01:29 +0800

    CoreData added

---
+++ SF Weather/DataModels/DataModel.swift
@@ -0,0 +1,9 @@
+//
+//  DataModel.swift
+//  SF Weather
+//
+//  Created by viewQwest on 05/05/2018.
+//  Copyright © 2018 sathyabaman. All rights reserved.
+//
+
+import Foundation
--- SF Weather/ViewController/DashBoard.swift
+++ SF Weather/ViewController/DashBoard.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreData
 
 class DashBoard: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
@@ -34,5 +35,50 @@ class DashBoard: UIViewController, UITableViewDelegate, UITableViewDataSource {
         return cell
     }
 
+    
+    @IBAction func buttonOrderList(_ sender: Any) {
+      //  saveItem()
+        
+        retrieveWatherData()
+    }
+    
+    func saveItem(){
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        
+        //**Note:** Here we are providing the entityName **`Entity`** that we have added in the model
+        let entity = NSEntityDescription.entity(forEntityName: "WeatherList",  in: context)
+
+       
+                let newWeather = NSManagedObject(entity: entity!, insertInto: context)
+                newWeather.setValue(1, forKey: "id")
+                newWeather.setValue("Kilinochchi", forKey: "cityName")
+                newWeather.setValue("SUNNY", forKey: "condition")
+                newWeather.setValue(23.2, forKey: "humidity")
+                newWeather.setValue(60, forKey: "maxTemperature")
+                newWeather.setValue(10, forKey: "minTemperature")
+        
+                do {
+                    try context.save()
+                } catch{
+                    print("There was an error in saving data")
+                }
+    }
+    
+    func retrieveWatherData(){
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "WeatherList")
+        
+        do {
+            let results = try context.fetch(fetchRequest)
+            let obtainedResults = results as! [NSManagedObject]
+            for i in 0 ..< obtainedResults.count{
+                let firstResult = obtainedResults[i]
+                let myValue = firstResult.value(forKey: "humidity")
+                print("Humidity: \(myValue)")
+            }
+        } catch {
+            print("Error")
+        }
+    }
 
 }
