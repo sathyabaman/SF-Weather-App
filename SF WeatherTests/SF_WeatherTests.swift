d8c60be5164da027faa55f96d4362a5b9576dcd3
sathya baman <sathyabaman.k@viewqwest.com>
2018-05-05 11:07:07 +0800

    initial commit

---
+++ SF WeatherTests/SF_WeatherTests.swift
@@ -0,0 +1,36 @@
+//
+//  SF_WeatherTests.swift
+//  SF WeatherTests
+//
+//  Created by viewQwest on 05/05/2018.
+//  Copyright © 2018 sathyabaman. All rights reserved.
+//
+
+import XCTest
+@testable import SF_Weather
+
+class SF_WeatherTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
